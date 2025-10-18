511df0ffd3361ef149dd7251dcd0e704ea066a85
thiagosullivan <thiagosullivan12@gmail.com>
2025-10-18 19:58:35 -0300

    feat: add chart to dashboard

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,16 +1,104 @@
+import { Link } from "react-router-dom";
 import React, { useState } from "react";
+import { Button, Checkbox, Pagination, Tooltip } from "@mui/material";
+
 import { DashboardBoxes } from "../../components/DashboardBoxes";
 import OrdersTable from "../../components/DashboardTable";
-import { Button, Checkbox, Pagination, Tooltip } from "@mui/material";
-import { FaPlus, FaRegEye } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import { ProgressBar } from "../../components/ProgressBar";
+
+import { FaPlus, FaRegEye } from "react-icons/fa";
 import { AiOutlineEdit } from "react-icons/ai";
 import { GoTrash } from "react-icons/go";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip as RechartTooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export const Dashboard = () => {
+  const [chart1Data, setChart1Data] = useState([
+    {
+      name: "Jan",
+      TotalSales: 1000,
+      TotalUsers: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Feb",
+      TotalSales: 2000,
+      TotalUsers: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Mar",
+      TotalSales: 3000,
+      TotalUsers: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Apr",
+      TotalSales: 4780,
+      TotalUsers: 3908,
+      amt: 2000,
+    },
+    {
+      name: "May",
+      TotalSales: 10890,
+      TotalUsers: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Jun",
+      TotalSales: 7390,
+      TotalUsers: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Jul",
+      TotalSales: 8490,
+      TotalUsers: 2150,
+      amt: 2100,
+    },
+    {
+      name: "Aug",
+      TotalSales: 2490,
+      TotalUsers: 4500,
+      amt: 2700,
+    },
+    {
+      name: "Sep",
+      TotalSales: 2490,
+      TotalUsers: 3120,
+      amt: 2550,
+    },
+    {
+      name: "Oct",
+      TotalSales: 3490,
+      TotalUsers: 3490,
+      amt: 2810,
+    },
+    {
+      name: "Nov",
+      TotalSales: 4490,
+      TotalUsers: 2200,
+      amt: 2010,
+    },
+    {
+      name: "Dec",
+      TotalSales: 8490,
+      TotalUsers: 5200,
+      amt: 2300,
+    },
+  ]);
+
   return (
     <>
       <div className="w-full p-5 border border-[rgba(0,0,0,0.2)] flex items-center gap-8 mb-5 rounded-md bg-white">
@@ -356,12 +444,62 @@ export const Dashboard = () => {
           <Pagination count={10} color="primary" />
         </div>
       </div>
-      <div className="bg-white p-5 border border-[rgba(0,0,0,0.2)] rounded-md">
+      <div className="bg-white p-5 border border-[rgba(0,0,0,0.2)] rounded-md mb-5">
         <h3 className="font-bold mb-5 text-2xl">Recent Orders</h3>
 
         <div>
           <OrdersTable />
         </div>
+      </div>
+
+      <div className="bg-white p-5 border border-[rgba(0,0,0,0.2)] rounded-md mb-5">
+        <h3 className="font-bold mb-5 text-2xl">Total Users & Total Sales</h3>
+
+        <div className="flex items-center justify-start px-5 py-5 pt-1 gap-6">
+          <div className="flex items-center gap-1 text-sm">
+            <span className="block w-[8px] h-[8px] rounded-full bg-[#82ca9d] mr-1"></span>
+            Total Users
+          </div>
+          <div className="flex items-center gap-1 text-sm">
+            <span className="block w-[8px] h-[8px] rounded-full bg-[#8884d8] mr-1"></span>
+            Total Sales
+          </div>
+        </div>
+        <LineChart
+          style={{
+            width: "100%",
+            maxWidth: "700px",
+            maxHeight: "70vh",
+            aspectRatio: 1.618,
+          }}
+          responsive
+          data={chart1Data}
+          margin={{
+            top: 5,
+            right: 0,
+            left: 0,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" stroke="none" />
+          <XAxis dataKey="name" tick={{ fontSize: 12 }} />
+          <YAxis width="auto" tick={{ fontSize: 12 }} />
+          <RechartTooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="TotalSales"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+            strokeWidth={3}
+          />
+          <Line
+            type="monotone"
+            dataKey="TotalUsers"
+            stroke="#82ca9d"
+            strokeWidth={3}
+          />
+        </LineChart>
       </div>
     </>
   );
